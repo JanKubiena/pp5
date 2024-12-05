6e5b527ac7262e901c4a5395af000f7021392069
JanKubiena <jan@kubiena.pl>
2024-12-05 22:27:46 +0100

    add product module and created routing for client and product

--- invoice-app/src/app/app.routes.ts
+++ invoice-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductFormComponent } from './product/components/product-form/product-form.component';
+import { ClientFormComponent } from './invoice/components/client-form/client-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'product-form', component: ProductFormComponent },
+  { path: 'client-form', component: ClientFormComponent },
+];
