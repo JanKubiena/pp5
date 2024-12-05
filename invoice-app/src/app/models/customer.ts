d97c96cee491293a426c739d13948e82fd7be637
JanKubiena <jan@kubiena.pl>
2024-12-05 14:01:28 +0100

    add enums and models to invoice-app

---
+++ invoice-app/src/app/models/customer.ts
@@ -0,0 +1,32 @@
+import { Invoice } from '../models/invoice';
+
+export class Customer {
+  constructor() {}
+
+  firmLabel: string = '';
+  nip: number = 0;
+  cityName: string = '';
+  streetName: string = '';
+  houseNumber: number = 0;
+  apartmentNumber: number = 0;
+  postalcode: string = '';
+  comments: string = '';
+  industry: string = '';
+  isActive: boolean = true;
+
+  getAddress(): any {
+    return `${this.cityName} ${this.postalcode} ${this.streetName} ${this.houseNumber}`;
+  }
+  getCustomerInfo(): any {
+    return `label: "${this.firmLabel}" nip: ${this.nip}`;
+  }
+}
+
+class Supplier extends Customer {
+  constructor(accountNumber: any) {
+    super();
+    accountNumber = accountNumber;
+  }
+  accountNumber: string = '';
+  invoices: Invoice[] = [];
+}
---
+++ invoice-app/src/app/models/invoice.ts
@@ -0,0 +1,10 @@
+export class Invoice {
+  constructor() {}
+
+  name: string = '';
+  quantity: number = 0;
+  unit?: Unit;
+  netto?: number;
+  tax?: Tax;
+  brutto?: number;
+}
